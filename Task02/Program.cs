265bb7bf0017d1e53a4535b6b27b1c982bc333d5
Liberaty <lepishin.ao@yandex.ru>
2022-08-11 01:19:14 +0500

    Task02 changed

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -15,20 +15,30 @@ int num1 = Prompt("Введите первое число: ");
 int num2 = Prompt("Введите второе число: ");
 int num3 = Prompt("Введите третье число: ");
 
-if (num1 >= num2 && num1 >= num3) {
+if (num1 >= num2 && num1 >= num3) 
+{
     max = num1;
-} else if (num2 >= num1 && num2 >= num3) {
+} 
+else if (num2 >= num1 && num2 >= num3) 
+{
     max = num2;
-} else if (num3 >= num1 && num3 >= num2) {
+} 
+else if (num3 >= num1 && num3 >= num2) 
+{
     max = num3;
 }
 // int maximal = Math.Max(Math.Max(num1, num2), num3); // можно ещё так
 
-if (num1 <= num2 && num1 <= num3) {
+if (num1 <= num2 && num1 <= num3) 
+{
     min = num1;
-} else if (num2 <= num1 && num2 <= num3) {
+} 
+else if (num2 <= num1 && num2 <= num3) 
+{
     min = num2;
-} else if (num3 <= num1 && num3 <= num2) {
+} 
+else if (num3 <= num1 && num3 <= num2) 
+{
     min = num3;
 }
 // int minimal = Math.Min(Math.Min(num1, num2), num3); // можно ещё так
