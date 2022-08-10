dcda9006efe45a045d943011cf12ec11dccb08ee
Liberaty <lepishin.ao@yandex.ru>
2022-08-11 01:43:07 +0500

    Task04

---
+++ Task04/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);                                                  // вывести сообщение
+    string readValue = Console.ReadLine();                                          // считывает строку с консоли
+    int result = int.Parse(readValue);                                              // преобразует строку в целое число
+    return result;                                                                  // возвращает результат
+}
+
+int num1 = Prompt("Введите число: ");
+
+for (int i = num1; i >= 1; i--)
+{
+    if (i % 2 == 0) 
+    {
+        Console.Write(i + " ");
+    } 
+}
